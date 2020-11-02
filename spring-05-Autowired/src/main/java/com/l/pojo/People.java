19a46bdb9bb5b017d2e3ab2a4223a342175c94b0
manjaro-L <1411205284@qq.com>
2020-11-02 10:55:50 +0800

    xml autowire

--- spring-05-Autowired/src/main/java/com/l/pojo/People.java
+++ spring-05-Autowired/src/main/java/com/l/pojo/People.java
@@ -1,7 +1,13 @@
 package com.l.pojo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class People {
+    @Autowired(required = false)
     private Cat cat;
+    @Autowired
+    @Qualifier("dog")
     private Dog dog;
     private String name;
 
@@ -18,23 +24,11 @@ public class People {
         return cat;
     }
 
-    public void setCat(Cat cat) {
-        this.cat = cat;
-    }
-
     public Dog getDog() {
         return dog;
     }
 
-    public void setDog(Dog dog) {
-        this.dog = dog;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
