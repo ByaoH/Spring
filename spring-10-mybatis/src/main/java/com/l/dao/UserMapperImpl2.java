39f00afc2178c24e0ce5ce05abcdc1fef38fa31d
manjaro-L <1411205284@qq.com>
2020-11-03 16:31:43 +0800

    方式二 通过 SqlSessionDaoSupport

---
+++ spring-10-mybatis/src/main/java/com/l/dao/UserMapperImpl2.java
@@ -0,0 +1,13 @@
+package com.l.dao;
+
+import com.l.pojo.User;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+import java.util.List;
+
+public class UserMapperImpl2 extends SqlSessionDaoSupport implements  UserMapper{
+
+    public List<User> selectUser() {
+        return getSqlSession().getMapper(UserMapper.class).selectUser();
+    }
+}
